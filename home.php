d56c10361e222e645b609c28358faaaa1d4541ae
ignacio matias joaquin ibarra lopez <nacho_ibarra_lopez@hotmail.com>
2019-08-23 22:49:52 -0400

    inicio

---
+++ home.php
@@ -0,0 +1,232 @@
+<?php
+include("superior.php");
+?>
+
+    <!-- ***** Hero Area Start ***** -->
+    <section class="hero-area">
+        <div class="hero-slides owl-carousel">
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide1.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>Sand Storm</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="elements.php">Elements</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide2.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>New York View</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem.
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide3.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>The Desert</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="blog.php">Blog</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide4.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>Mountains Hike</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="portfolio.php">Portfolio</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide1.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>Sand Storm</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="elements.php">Elements</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide2.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>New York View</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem.
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide3.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>The Desert</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="blog.php">Blog</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide4.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>Mountains Hike</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="portfolio.php">Portfolio</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide1.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>Sand Storm</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="elements.php">Elements</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide2.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>New York View</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem.
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide3.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>The Desert</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="blog.php">Blog</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img slide-background-overlay"
+                style="background-image: url(img/bg-img/slide4.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-end">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <div class="line"></div>
+                                <h2>Mountains Hike</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel tortor
+                                    facilisis, volutpat nulla placerat, tincidunt mi. Nullam vel orci dui. Suspendisse
+                                    sit amet laoreet neque. Fusce sagittis suscipit sem. <a class="nav-link" href="portfolio.php">Portfolio</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+    <!-- ***** Hero Area End ***** -->
+<?php
+include("inferior.php");
+?>
