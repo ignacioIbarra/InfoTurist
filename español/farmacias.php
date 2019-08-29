1c8a3c0a49ef8e5c2c927903231d2165cd0cc352
ignacio matias joaquin ibarra lopez <nacho_ibarra_lopez@hotmail.com>
2019-08-28 23:05:09 -0400

    imagenes

---
+++ español/farmacias.php
@@ -0,0 +1,142 @@
+<?php
+include("../superior.php");
+?>
+<!-- ****** Gallery Area Start ****** -->
+<section class="sonar-projects-area" id="projects">
+    <div class="container-fluid">
+        <div class="row sonar-portfolio">
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes studio wow fadeInUpBig" data-wow-delay="300ms">
+                <a class="gallery-img" href="img/portfolio-img/1.jpg"><img src="../img/portfolio-img/1.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>Mountains in the mist</h4>
+                    <p>Landscapes</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits fashion wow fadeInUpBig" data-wow-delay="500ms">
+                <a class="gallery-img" href="img/portfolio-img/2.jpg"><img src="../img/portfolio-img/2.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>A beautifull encounter</h4>
+                    <p>Portrait</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes studio wow fadeInUpBig" data-wow-delay="700ms">
+                <a class="gallery-img" href="img/portfolio-img/3.jpg"><img src="../img/portfolio-img/3.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>Mountains in the mist</h4>
+                    <p>Landscapes</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits studio wow fadeInUpBig" data-wow-delay="900ms">
+                <a class="gallery-img" href="img/portfolio-img/4.jpg"><img src="../img/portfolio-img/4.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>A beautifull encounter</h4>
+                    <p>Portrait</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes fashion wow fadeInUpBig" data-wow-delay="300ms">
+                <a class="gallery-img" href="img/portfolio-img/5.jpg"><img src="../img/portfolio-img/5.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>Mountains in the mist</h4>
+                    <p>Landscapes</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits fashion wow fadeInUpBig" data-wow-delay="500ms">
+                <a class="gallery-img" href="img/portfolio-img/6.jpg"><img src="../img/portfolio-img/6.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>A beautifull encounter</h4>
+                    <p>Portrait</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes fashion wow fadeInUpBig" data-wow-delay="700ms">
+                <a class="gallery-img" href="img/portfolio-img/7.jpg"><img src="../img/portfolio-img/7.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>Mountains in the mist</h4>
+                    <p>Landscapes</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits studio wow fadeInUpBig" data-wow-delay="900ms">
+                <a class="gallery-img" href="img/portfolio-img/8.jpg"><img src="../img/portfolio-img/8.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>A beautifull encounter</h4>
+                    <p>Portrait</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes studio wow fadeInUpBig" data-wow-delay="300ms">
+                <a class="gallery-img" href="img/portfolio-img/9.jpg"><img src="../img/portfolio-img/9.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>Mountains in the mist</h4>
+                    <p>Landscapes</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="500ms">
+                <a class="gallery-img" href="img/portfolio-img/10.jpg"><img src="../img/portfolio-img/10.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>A beautifull encounter</h4>
+                    <p>Portrait</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes wow fadeInUpBig" data-wow-delay="700ms">
+                <a class="gallery-img" href="img/portfolio-img/11.jpg"><img src="../img/portfolio-img/11.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>Mountains in the mist</h4>
+                    <p>Landscapes</p>
+                </div>
+            </div>
+
+            <!-- Single gallery Item -->
+            <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
+                <a class="gallery-img" href="img/portfolio-img/12.jpg"><img src="../img/portfolio-img/12.jpg" alt=""></a>
+                <!-- Gallery Content -->
+                <div class="gallery-content">
+                    <h4>A beautifull encounter</h4>
+                    <p>Portrait</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 text-center">
+                <a href="#" class="btn sonar-btn">Load More</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ****** Gallery Area End ****** -->
+
+
+<?php
+include("../inferior.php");
+?>
