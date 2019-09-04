e0113d6e7cff262dfd6b29db63ba8ac22601807b
Felipe Besoain <fbesoain@interside.cl>
2019-09-04 00:15:18 -0400

    Auto stash before merge of "desarrollo" and "origin/desarrollo"

--- español/restaurantes.service.php
+++ español/restaurantes.service.php
@@ -1,4 +1,58 @@
 <?php
+$hamburguesas='Hamburguesas'; $estadoUnidense= 'Comida Estado Unidense'; $ceviche='Ceviche'; $tragos='Tragos';
+$fajitas ='Fajitas'; $pizza = 'Pizza'; $sandwich = 'Sandwich'; $hotdogs ='Completos (hotdogs)'; $pastas = 'Pastas';
+$postres = 'Postres';
+function filtrar($filtro){
+    global $restaurantes;
+    if($filtro == 0 ){
+        // todos
+        return $restaurantes;
+        
+    } else if($filtro == 1){
+         // chilena
+         return [$restaurantes[0],$restaurantes[8],$restaurantes[9],$restaurantes[12],$restaurantes[13],$restaurantes[6],
+            $restaurantes[15],$restaurantes[16],$restaurantes[17],$restaurantes[18],$restaurantes[19],$restaurantes[21],
+            $restaurantes[26],$restaurantes[29],$restaurantes[30],$restaurantes[32],$restaurantes[36]];
+        
+    } else if ($filtro == 2){
+       // comida rapida
+       return [$restaurantes[0],$restaurantes[1],$restaurantes[2],$restaurantes[5],$restaurantes[11],$restaurantes[22],
+       $restaurantes[26],$restaurantes[27],$restaurantes[31]
+        ,$restaurantes[23]];
+        
+    } else if ($filtro == 3){
+        // Parrilladas
+        return [$restaurantes[12],$restaurantes[13],$restaurantes[15],$restaurantes[18]];
+
+    } else if ($filtro == 4){
+        // comida internacional
+        return [$restaurantes[0],$restaurantes[8],$restaurantes[10],$restaurantes[12],$restaurantes[14],
+            $restaurantes[16],$restaurantes[17],$restaurantes[19],$restaurantes[20],$restaurantes[21],
+            $restaurantes[27],$restaurantes[29],$restaurantes[32]
+            ,$restaurantes[33],$restaurantes[34],$restaurantes[36],$restaurantes[37],$restaurantes[39]];
+
+    } else if ($filtro == 5){
+        // Bar
+        return [$restaurantes[8],$restaurantes[14],$restaurantes[22],$restaurantes[27],$restaurantes[30],$restaurantes[32],$restaurantes[39]];
+        
+    } else if ($filtro == 6){
+        // cafe
+        return [$restaurantes[0],$restaurantes[9],$restaurantes[10],$restaurantes[11],$restaurantes[19],
+        $restaurantes[28],$restaurantes[31],$restaurantes[33],$restaurantes[34],$restaurantes[36],$restaurantes[37]];
+    } else if($filtro == 7){
+        // vegana
+        return [$restaurantes[3],$restaurantes[4],$restaurantes[6],$restaurantes[7],
+        $restaurantes[20],$restaurantes[24],$restaurantes[28],$restaurantes[38],$restaurantes[25]];
+    } else if($filtro == 8){
+        // gastropub
+        return [$restaurantes[1],$restaurantes[14],$restaurantes[27],
+        $restaurantes[28],$restaurantes[30]];
+    } else if ($filtro == 9){
+        // delicatense
+        return [$restaurantes[10],$restaurantes[26],$restaurantes[28],$restaurantes[31],$restaurantes[33],$restaurantes[34],$restaurantes[35]];
+    }
+};
+
 $res1 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item landscapes studio wow fadeInUpBig" data-wow-delay="300ms" id="1">
         <a class="gallery-img" href="../images/restaurantes/variedad/torreon.jpg"><img src="../images/restaurantes/variedad/torreon.jpg" alt=""></a>
@@ -12,7 +66,7 @@ $res2 = '<!-- Single gallery Item -->
         <a class="gallery-img" href="../images/restaurantes/variedad/beanies.jpg"><img src="../images/restaurantes/variedad/beanies.jpg" alt=""></a>
         <!-- Gallery Content -->
         <div class="gallery-content">
-        <h4>BEANIES & BIKINIS</h4>
+        <h4>Beanis & Bikins </h4>
         </div>
     </div>';
 
@@ -27,7 +81,7 @@ $res3 = '<!-- Single gallery Item -->
 
 $res4 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits studio wow fadeInUpBig" data-wow-delay="900ms">
-        <a class="gallery-img" href="../images/restaurantes/variedad/trattoria.jpg"><img src="../images/restaurantes/variedad/trattoria.jpg" alt=""></a>
+        <a href="http://localhost/infoTurist/espa%C3%B1ol/detalle-restaurant.php"><img src="../images/restaurantes/variedad/trattoria.jpg" alt=""></a>
         <!-- Gallery Content -->
         <div class="gallery-content">
             <h4>La Trattoria da Pietro</h4>
@@ -55,7 +109,7 @@ $res7 = '<!-- Single gallery Item -->
         <a class="gallery-img" href="../images/restaurantes/variedad/andiamo.jpg"><img src="../images/restaurantes/variedad/andiamo.jpg" alt=""></a>
         <!-- Gallery Content -->
         <div class="gallery-content">
-            <h4>Andiamo - Cucina Italiana</h4>
+            <h4>Andiamo</h4>
         </div>
     </div>';
 $res8 = '<!-- Single gallery Item -->
@@ -266,7 +320,7 @@ $res30 = '<!-- Single gallery Item -->
 
 $res31 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\sangucheriaPata.jpg"><img src="../images\restaurantes\variedad\sangucheriaPata.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/sangucheriaPata.jpg"><img src="../images/restaurantes/variedad/sangucheriaPata.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Sangucheria Patacon</h4>
@@ -275,7 +329,7 @@ $res31 = '<!-- Single gallery Item -->
 
 $res32 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\almacen.jpg"><img src="../images\restaurantes\variedad\almacen.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/almacen.jpg"><img src="../images/restaurantes/variedad/almacen.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Almacén de Quetro</h4>
@@ -284,7 +338,7 @@ $res32 = '<!-- Single gallery Item -->
 
 $res33 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\ottoBar.jpg"><img src="../images\restaurantes\variedad\ottoBar.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/ottoBar.jpg"><img src="../images/restaurantes/variedad/ottoBar.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Otto Bar</h4>
@@ -293,7 +347,7 @@ $res33 = '<!-- Single gallery Item -->
 
 $res34 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\klee.jpg"><img src="../images\restaurantes\variedad\klee.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/klee.jpg"><img src="../images/restaurantes/variedad/klee.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Klee Kaffee</h4>
@@ -302,7 +356,7 @@ $res34 = '<!-- Single gallery Item -->
 
 $res35 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-        <a class="gallery-img" href="../images\restaurantes\variedad\kuchenladen.jpg"><img src="../images\restaurantes\variedad\kuchenladen.jpg" alt=""></a>
+        <a class="gallery-img" href="../images/restaurantes/variedad/kuchenladen.jpg"><img src="../images/restaurantes/variedad/kuchenladen.jpg" alt=""></a>
         <!-- Gallery Content -->
         <div class="gallery-content">
             <h4>Kuchenladen</h4>
@@ -311,7 +365,7 @@ $res35 = '<!-- Single gallery Item -->
 
 $res36 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-        <a class="gallery-img" href="../images\restaurantes\variedad\torreDelArtista.jpg"><img src="../images\restaurantes\variedad\torreDelArtista.jpg" alt=""></a>
+        <a class="gallery-img" href="../images/restaurantes/variedad/torreDelArtista.jpg"><img src="../images/restaurantes/variedad/torreDelArtista.jpg" alt=""></a>
         <!-- Gallery Content -->
         <div class="gallery-content">
             <h4>Torre del artista</h4>
@@ -320,7 +374,7 @@ $res36 = '<!-- Single gallery Item -->
 
 $res37 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\297cafe.jpg"><img src="../images\restaurantes\variedad\297cafe.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/297cafe.jpg"><img src="../images/restaurantes/variedad/297cafe.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>297 Café Bistro</h4>
@@ -329,7 +383,7 @@ $res37 = '<!-- Single gallery Item -->
 
 $res38 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\cafeBerlin.jpg"><img src="../images\restaurantes\variedad\cafeBerlin.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/cafeBerlin.jpg"><img src="../images/restaurantes/variedad/cafeBerlin.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Café Berlin</h4>
@@ -338,7 +392,7 @@ $res38 = '<!-- Single gallery Item -->
 
 $res39 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\senzo.jpg"><img src="../images\restaurantes\variedad\senzo.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/senzo.jpg"><img src="../images/restaurantes/variedad/senzo.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Senzo</h4>
@@ -347,72 +401,34 @@ $res39 = '<!-- Single gallery Item -->
 
 $res40 = '<!-- Single gallery Item -->
     <div class="col-12 col-sm-6 col-lg-3 single_gallery_item portraits wow fadeInUpBig" data-wow-delay="900ms">
-    <a class="gallery-img" href="../images\restaurantes\variedad\latitude39.jpg"><img src="../images\restaurantes\variedad\latitude39.jpg" alt=""></a>
+    <a class="gallery-img" href="../images/restaurantes/variedad/latitude39.jpg"><img src="../images/restaurantes/variedad/latitude39.jpg" alt=""></a>
     <!-- Gallery Content -->
     <div class="gallery-content">
         <h4>Latitude 39</h4>
     </div>
     </div>';
 
-$restaurantes = [
-    $res1, $res2, $res3, $res4, $res5, $res6, $res7, $res8, $res9, $res10, $res11, $res12, $res13,
-    $res14, $res15, $res16, $res17, $res18, $res19, $res20, $res21, $res22, $res23, $res24, $res25, $res26,
-    $res27, $res28, $res29, $res30, $res31, $res32, $res33, $res34, $res35, $res36, $res37, $res38, $res39, $res40,
-];
-
-function filtrar($filtro)
-{
-    global $restaurantes;
-    if ($filtro == 0) {
-        // todos
-        return $restaurantes;
-    } else if ($filtro == 1) {
-        // chilena
-        return [
-            $restaurantes[0], $restaurantes[8], $restaurantes[9], $restaurantes[12], $restaurantes[13],
-            $restaurantes[15], $restaurantes[16], $restaurantes[17], $restaurantes[18], $restaurantes[19], $restaurantes[21],
-            $restaurantes[26], $restaurantes[29], $restaurantes[30], $restaurantes[32], $restaurantes[36]
-        ];
-    } else if ($filtro == 2) {
-        // comida rapida
-        return [
-            $restaurantes[0], $restaurantes[1], $restaurantes[2], $restaurantes[5], $restaurantes[11], $restaurantes[22],
-            $restaurantes[26], $restaurantes[27], $restaurantes[31], $restaurantes[23]
-        ];
-    } else if ($filtro == 3) {
-        // Parrilladas
-        return [$restaurantes[12], $restaurantes[13], $restaurantes[15], $restaurantes[18]];
-    } else if ($filtro == 4) {
-        // comida internacional
-        return [
-            $restaurantes[0], $restaurantes[8], $restaurantes[10], $restaurantes[12], $restaurantes[14],
-            $restaurantes[16], $restaurantes[17], $restaurantes[19], $restaurantes[20], $restaurantes[21],
-            $restaurantes[27], $restaurantes[29], $restaurantes[32], $restaurantes[33], $restaurantes[34], $restaurantes[36], $restaurantes[37], $restaurantes[39]
-        ];
-    } else if ($filtro == 5) {
-        // Bar
-        return [$restaurantes[8], $restaurantes[14], $restaurantes[22], $restaurantes[27], $restaurantes[30], $restaurantes[32], $restaurantes[39]];
-    } else if ($filtro == 6) {
-        // cafe
-        return [
-            $restaurantes[0], $restaurantes[9], $restaurantes[10], $restaurantes[11], $restaurantes[19],
-            $restaurantes[28], $restaurantes[31], $restaurantes[33], $restaurantes[34], $restaurantes[36], $restaurantes[37]
-        ];
-    } else if ($filtro == 7) {
-        // vegana
-        return [
-            $restaurantes[3], $restaurantes[4], $restaurantes[7],
-            $restaurantes[20], $restaurantes[24], $restaurantes[28], $restaurantes[38], $restaurantes[25]
-        ];
-    } else if ($filtro == 8) {
-        // gastropub
-        return [
-            $restaurantes[1], $restaurantes[14], $restaurantes[27],
-            $restaurantes[28], $restaurantes[30]
-        ];
-    } else if ($filtro == 9) {
-        // delicatense
-        return [$restaurantes[10], $restaurantes[26], $restaurantes[28], $restaurantes[31], $restaurantes[33], $restaurantes[34], $restaurantes[35]];
-    }
-};
-?>
+$restaurantes = [$res1,$res2,$res3,$res4,$res5,$res6, $res7,$res8,$res9,$res10,$res11,$res12,$res13, $res14,$res15,$res16,$res17,$res18,$res19,$res20,$res21,$res22,$res23,$res24,$res25,$res26,
+    $res27,$res28,$res29,$res30,$res31,$res32,$res33,$res34,$res35,$res36,$res37,$res38,$res39,$res40,];
+    $c1='Chilena';
+    $c2 = 'Comida rápida';
+    $c3= 'Parrilladas';
+    $c4 = 'Internacional';
+    $c5 = 'Bar';
+    $c6 = 'Café';
+    $c7 = 'Vegana';
+    $c8 = 'Gastropub';
+    $c9 = 'Delicatense';
+    
+    $comRest1=[$c1,$c2,$c4,$c6];  $comRest2 =[$c8];  $comRest3 =[$c2];  $comRest4 =[$c7]; $comRest5 =[$c7]; $comRest6 =[$c2]; $comRest7 =[$c1,$c7];
+     $comRest8 =[$c7];   $comRest9 =[$c1,$c4,$c5];  $comRest10 =[$c1,$c6]; $comRest11 =[$c4,$c6,$c9]; $comRest12 =[$c2,$c6]; $comRest13 =[$c1,$c3,$c4];
+    $comRest14 =[$c1,$c3]; $comRest15 =[$c4,$c5,$c8];  $comRest16 =[$c1,$c3]; $comRest17 =[$c1,$c4]; $comRest18 =[$c1,$c4]; $comRest19 =[$c1,$c3];
+    $comRest20 =[$c1,$c4,$c6]; $comRest21 =[$c4,$c7]; $comRest22 =[$c1,$c4]; $comRest23 =[$c2,$c5]; $comRest24 =[$c2];$comRest25 =[$c7];
+    $comRest26 =[$c7]; $comRest27 =[$c1,$c2,$c9]; $comRest28 =[$c2,$c4,$c8]; $comRest29 =[$c6,$c7,$c8,$c9]; $comRest30 =[$c1,$c4];
+    $comRest31 =[$c1,$c5,$c8]; $comRest32 =[$c2,$c6,$c9]; $comRest33 =[$c1,$c4,$c5]; $comRest34 =[$c4,$c6,$c9]; $comRest35 =[$c4,$c6,$c9];
+    $comRest36 =[$c9]; $comRest37 =[$c1,$c4,$c6]; $comRest38 =[$c4,$c6];  $comRest39 =[$c7]; $comRest40 =[$c4,$c5];
+    
+    $tiposComidaArr =[$comRest1,$comRest2,$comRest3,$comRest4,$comRest5,$comRest6,$comRest7,$comRest8,$comRest9,$comRest10,$comRest11,$comRest12,$comRest13,$comRest14,$comRest15,
+    $comRest16,$comRest17,$comRest18,$comRest19,$comRest20,$comRest21,$comRest22,$comRest23,$comRest24,$comRest25,$comRest26,$comRest27,$comRest28,$comRest29,$comRest30,$comRest31,
+    $comRest32,$comRest33,$comRest34,$comRest35,$comRest36,$comRest37,$comRest38,$comRest39,$comRest40];
+    
